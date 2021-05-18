eb724a3377e4a44a01466100e32be992dcd01e1f
Luis Freire D'Andrade <luis.vilhena@tecnico.ulisboa.pt>
2021-05-18 14:11:30 +0100

    Undo

--- sketch.js
+++ sketch.js
@@ -549,7 +549,7 @@ function printAndSavePerformance()
     // Initialize our Firebase
     if (attempt === 0)
     {
-    firebase.initializeApp(myFirebaseConfig);
+    firebase.initializeApp(ourFirebase);
     database = firebase.database();
     }
     let ref = database.ref('Scores/'+ ITERATION + '/' + student_ID);
